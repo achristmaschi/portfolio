a3f3e431c0151acca8d0bcdb9073d2a8b32cea70
Alan Tran <alan.tran@wisetechglobal.com>
2026-03-14 16:28:18 +1100

    setup git

---
+++ setup-git.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git config --global user.name "achristmaschi"
+git config --global user.email "chitg.tran@gmail.com"
+echo "✅ Git configured: $(git config --global user.name) <$(git config --global user.email)>"
